240bc86f19eba7ecd64b83988b02e2f980c0d72c
jcayouette <jcayouette@suse.com>
2018-06-05 16:45:40 +0200

    testing vhms

--- docmodules/virtual_host_manager.js
+++ docmodules/virtual_host_manager.js
@@ -9,19 +9,21 @@ casper.thenOpen(url + '/rhn/manager/vhms', function() {
     this.captureSelector('images/systems/systems_virtual_host_managers.png', '#spacewalk-content')
 });
 
-// Systems: Virtual Host Managers
+// Systems: VHMS File
 casper.thenOpen(url + '/rhn/manager/vhms#/create/file', function() {
     this.captureSelector('images/systems/systems_virtual_host_managers_file.png', '#spacewalk-content')
 });
 
-// Systems: Virtual Host Managers
+// Systems: VHMS VMWare
 casper.thenOpen(url + '/rhn/manager/vhms#/create/vmware', function() {
     //use sendKeys by default for filling forms. If there is javascript the dict function will not work.
+   /*
     this.sendKeys("input[name='label']", 'Galaxy');
     this.sendKeys("input[name='module_hostname']", 'galaxy.suse.com');
     this.sendKeys("input[name='module_port']", '443');
     this.sendKeys("input[name='module_username']", 'Geeko');
     this.sendKeys("input[name='module_password']", 'Penguin');
+    */
     this.captureSelector('images/systems/systems_virtual_host_managers_vmware.png', '#spacewalk-content');
 
 });
